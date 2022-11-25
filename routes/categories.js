3a78c596d5dd674f63069400b975ef9af084a9b9
Koklev <danikoklef@gmail.com>
2022-11-25 18:09:51 +0200

    second commit

--- routes/categories.js
+++ routes/categories.js
@@ -1,5 +1,6 @@
 const {Category} = require('../models/category');
 const express = require('express');
+const e = require('express');
 const router = express.Router();
 
 router.get(`/`, async (req, res) =>{
@@ -8,7 +9,59 @@ router.get(`/`, async (req, res) =>{
     if(!categoryList) {
         res.status(500).json({success: false})
     } 
-    res.send(categoryList);
+    res.status(200).send(categoryList);
 })
 
-module.exports =router;
+router.get('/', async (req, res) =>{
+    const category = await Category.findById(req.params.id);
+    if(!category){
+        res.status(500).json({message: "The category with the given ID was not found."});
+    }
+    res.status(200).send(category);
+})
+
+router.post('/', async (req, res) =>{
+    let category = new Category({
+        name: req.body.name,
+        icon: req.body.icon,
+        color: req.body.color
+    })
+    category = await category.save();
+
+    if (!category){
+        return res.status(404).send('the category cannot be created');
+    } else {
+        res.send(category);
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    const category = await Category.findByIdAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            icon: req.body.icon,
+            color: req.body.color
+        }
+    )
+    
+    if (!category){
+        return res.status(404).send('the category cannot be created');
+    } else {
+        res.send(category);
+    }
+})
+
+router.delete('/:id', (req, res) => {
+    Category.findByIdAndRemove(req.params.id).then(category =>{
+        if(category){
+            return res.status(200).json({sucess: true, message: "the category is deleted"});
+        } else {
+            return res.status(404).json({success: false, message: "category cannot be found"});
+        }
+    }).catch(err => {
+        return res.status(400).json({success: false, error: err});
+    })
+})
+
+module.exports = router;
